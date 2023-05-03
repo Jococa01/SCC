46c1d11c363bd8b214b1edd5a0d80d33fa2b1f72
Joan Coronado Casades <jococa01@floridauniversitaria.es>
2023-05-03 14:00:51 +0200

    Ranking html & API calls update

--- SCC_APP/src/app/views/team/team.component.ts
+++ SCC_APP/src/app/views/team/team.component.ts
@@ -26,14 +26,23 @@ export class TeamComponent {
     let newArray = [];
     console.log(response);
     this.TeamName = response[0].NAME;
-    for(let i = 0; i<response[0].PLAYERS.length; i++){
-      newArray[i] = [response[0].PLAYERS[i].ID,response[0].PLAYERS[i].NICK,response[0].PLAYERS[i].NAME,response[0].PLAYERS[i].FLAG];
-    }
-    if(newArray.length<nPlayers){
-      for(let i = newArray.length; i<nPlayers; i++){
+    this.TeamLogo = response[0].LOGO;
+    console.log(this.TeamLogo);
+    if(response[0].PLAYERS != "none"){
+      for(let i = 0; i<response[0].PLAYERS.length; i++){
+        newArray[i] = [response[0].PLAYERS[i].ID,response[0].PLAYERS[i].NICK,response[0].PLAYERS[i].NAME,response[0].PLAYERS[i].FLAG];
+      }
+      if(newArray.length<nPlayers){
+        for(let i = newArray.length; i<nPlayers; i++){
+          newArray.push(["","?","",""]);
+        }
+      }
+    }else{
+      for(let n = 0; n<nPlayers; n++){
         newArray.push(["","?","",""]);
       }
     }
+
     this.Players = newArray;
     console.log(this.Players);
     
@@ -41,12 +50,8 @@ export class TeamComponent {
 
 
   ngOnInit() {
-    // Note: Below 'queryParams' can be replaced with 'params' depending on your requirements
-    this.activatedRoute.queryParams.subscribe(params => {
-        this.TeamID= params['id'];
-        console.log("La id del equipo es "+this.TeamID);
-        this.getPlayers(this.TeamID);
-      });
+    this.TeamID = this.activatedRoute.snapshot.params['id'];
+    this.getPlayers(this.TeamID);
   }
 
 }
